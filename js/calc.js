73c1dd76e14a5e0a98e609ab81f8923a90b5fd6a
Jim Banks <iamjimhbanks@me.com>
2015-05-05 18:49:47 +0100

    added first number function

---
+++ js/calc.js
@@ -0,0 +1,43 @@
+// var clickNumber = document.getElementsByClassName('number');
+
+// clickButton.addEventListener('click', function(event){
+// });
+
+// var numbers = document.getElementsByClassName('number');
+// console.log(numbers);
+
+function getFirstNumber(number) {
+  firstNum = Number(number);
+   
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
