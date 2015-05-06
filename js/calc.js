a03857495d2b37caea511ce24a194c80b10d34ab
Jim Banks <iamjimhbanks@me.com>
2015-05-06 07:05:12 +0100

    added class id to the var clear

--- js/calc.js
+++ js/calc.js
@@ -4,36 +4,44 @@ var operator = document.getElementsByClassName("operator")
 
 var equals = document.getElementById('eq');
  equals.addEventListener('click', function(event){
-// console.log(this.value);
+console.log(this.value);
  });
 
 var clear = document.getElementById('clear');
  clear.addEventListener('click', function(event){
-// console.log(this.value);
+console.log(this.value);
+return this.value;
  });
 
 
 for(var i=0;i<number.length;i++){
 number[i].addEventListener('click', function(event){
-  // console.log(this.value);
+  console.log(this.value);
 });
 }
 
 for(var j=0;j<operator.length;j++){
  operator[j].addEventListener('click', function(event){
  var oper = this.value;
- console.log.(oper);
+ // console.log.(oper);
  return.this.value;
- // console.log(this.value);
+ console.log(this.value);
  return.oper
  }});
 }
 
 var addition function('num1', 'num1', 'operator')
-if (operator === '+') {
+    if (operator === '+') {
+      var sum = num1 + num2;
+}
+console.log(sum);
+debugger 
 
 
 }   
+
+
+
 
 
 
