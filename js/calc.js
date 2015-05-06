103032352a4fa03a8469fd3828f4083300fdb75b
Jim Banks <iamjimhbanks@me.com>
2015-05-06 08:34:45 +0100

    added more notes

--- js/calc.js
+++ js/calc.js
@@ -5,18 +5,20 @@ var number = document.getElementsByClassName("number");
 // get operator using "operator" id
 var operator = document.getElementsByClassName("operator")
 
+// get by id ('eq' is this case), then use the eventlistener to reg 'click' 
 var equals = document.getElementById('eq');
  equals.addEventListener('click', function(event){
 console.log(this.value);
  });
 
+// get by id ('clear' is this case), then use the eventlistener to reg 'click'.  Return value.  Added clear as id to html
 var clear = document.getElementById('clear');
  clear.addEventListener('click', function(event){
 console.log(this.value);
 return this.value;
  });
 
-
+// 
 for(var i=0;i<number.length;i++){
 number[i].addEventListener('click', function(event){
   console.log(this.value);
@@ -28,7 +30,7 @@ for(var j=0;j<operator.length;j++){
  var oper = this.value;
  // console.log.(oper);
  return.this.value;
- console.log(this.value);
+   console.log(this.value);
  return.oper
  }});
 }
